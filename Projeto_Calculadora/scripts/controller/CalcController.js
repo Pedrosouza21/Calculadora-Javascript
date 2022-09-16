5fe914c96d2af5f92415b67880b4d8d3e22b51a7
Pedrosouza21 <pedrohenriqueoliveiradesouza9@gmail.com>
2022-09-16 08:32:35 -0300

    validating the first operator with Eval

--- Projeto_Calculadora/scripts/controller/CalcController.js
+++ Projeto_Calculadora/scripts/controller/CalcController.js
@@ -39,17 +39,45 @@ class CalcController {
     }
 
     getLastOperation() {
-        return this._operation[this._operation.length-1];
+        return this._operation[this._operation.length - 1];
 
 
     }
 
     setLastOperation(value) {
-        this._operation[this._operation.length-1] = value;
+        this._operation[this._operation.length - 1] = value;
     }
 
     isOperator(value) {
         return (['+', '-', '*', '%', '/'].indexOf(value) > -1);
+    }
+
+
+    pushOperation(value) {
+        this._operation.push(value);
+
+        if (this._operation.length > 3) {
+
+            this.calc();
+
+        }
+
+    }
+
+    //Utilizando Evall
+
+    calc() {
+        let last = this._operation.pop();
+
+        let result = eval(this._operation.join(""));
+
+        this._operation = [result, last];
+
+    }
+
+    setLastNumeberToDisplay(){
+
+        
     }
 
 
@@ -59,165 +87,178 @@ class CalcController {
         if (isNaN(this.getLastOperation())) {
 
             if (this.isOperator(value)) {
-                this._setLastOperation(value);
+                this.setLastOperation(value);
                 //trocar Operador
 
             } else if (isNaN(value)) {
                 console.log(value);
 
             } else {
-                this._operation.push(value);
+                this.pushOperation(value);
 
-                console.log(value);
             }
+
+
         } else {
-        let newValue = this.setLastOperation().toString() + value.toString();
-        //Number 
-        this.setLastOperation(parseInt(nemValue));
+
+            if (this.isOperator(value)) {
+                this.pushOperation(value);
+
+            } else {
+                let newValue = this.getLastOperation().toString() + value.toString();
+                this.setLastOperation(parseInt(nemValue));
+
+                this.setLastNumeberToDisplay();
+
+                //Atualizar Display
+
+            }
+
         }
+
         console.log(this.operation)
-    
-  
-}
 
-
-setError() {
-    this.displayCalc = "Error";
-
-}
-
-/* addEventListener recebe 2 parametros(cliques) */
-/* switch = se for feito x, faça y e pare
-break = pare
-Se clicar em ac, irá apagar tudo */
-
-/* Incrementando switch e case*/
-execBtn(value) {
-    switch (value) {
-        case 'ac':
-            this.clearAll();
-            break;
-
-        case 'ce':
-            this.clearEntry();
-            break;
-
-        case 'soma':
-            this.addOperation(parseInt('+'));
-            break;
-
-        case 'subtracao':
-            this.addOperation(parseInt('-'));
-            break;
-
-        case 'divisao':
-            this.addOperation(parseInt('/'));
-            break;
-
-        case 'multiplicacao':
-            this.addOperation(parseInt('*'));
-            break;
-
-        case 'porcento':
-            this.addOperation(parseInt('%'));
-            break;
-
-        case 'igual':
-
-            break;
-
-        case 'ponto':
-            this.addOperation(parseInt('.'));
-
-            break;
-
-        case '0':
-        case '1':
-        case '2':
-        case '3':
-        case '4':
-        case '5':
-        case '6':
-        case '7':
-        case '8':
-        case '9':
-            this.addOperation(parseInt(value));
-            break;
-
-        default:
-            this.setError();
-            break;
 
     }
 
-}
 
-addEventListenerAll(element, events, fn) {
-    events.split(' ').forEach(event => {
-        element.addEventListener(event, fn, false);
-    });
+    setError() {
+        this.displayCalc = "Error";
 
+    }
 
-}
+    /* addEventListener recebe 2 parametros(cliques) */
+    /* switch = se for feito x, faça y e pare
+    break = pare
+    Se clicar em ac, irá apagar tudo */
 
+    /* Incrementando switch e case*/
+    execBtn(value) {
+        switch (value) {
+            case 'ac':
+                this.clearAll();
+                break;
 
-initButtonsEvents() {
-    let buttons = document.querySelectorAll("#buttons > g, #parts > g");
+            case 'ce':
+                this.clearEntry();
+                break;
 
-    buttons.forEach((btn, index) => {
+            case 'soma':
+                this.addOperation(parseInt('+'));
+                break;
 
-        this.addEventListenerAll(btn, "click drag mouseover", e => {
-            let textBtn = btn.className.baseVal.replace("btn-", "");
+            case 'subtracao':
+                this.addOperation(parseInt('-'));
+                break;
 
-            this.execBtn();
+            case 'divisao':
+                this.addOperation(parseInt('/'));
+                break;
+
+            case 'multiplicacao':
+                this.addOperation(parseInt('*'));
+                break;
+
+            case 'porcento':
+                this.addOperation(parseInt('%'));
+                break;
+
+            case 'igual':
+
+                break;
+
+            case 'ponto':
+                this.addOperation(parseInt('.'));
+
+                break;
+
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                this.addOperation(parseInt(value));
+                break;
+
+            default:
+                this.setError();
+                break;
+
+        }
+
+    }
+
+    addEventListenerAll(element, events, fn) {
+        events.split(' ').forEach(event => {
+            element.addEventListener(event, fn, false);
         });
 
-    });
 
-    this.addEventListenerAll(btn, "mouseover mouseup mousedown", e => {
-        btn, style.cursor = "pointer";
-    });
-
-}
+    }
 
 
-/* acrescentando método Set criado */
-setDisplayDateTime() {
-    this.displayDate = this.currentDate.toLocaleDateString(this._locale, {
-        day: "2-digit",
-        month: "long",
-        year: "numeric"
-    });
-    this.displayTime = this.currentDate.toLocaleDateString(this._locale);
+    initButtonsEvents() {
+        let buttons = document.querySelectorAll("#buttons > g, #parts > g");
 
-}
+        buttons.forEach((btn, index) => {
+
+            this.addEventListenerAll(btn, "click drag mouseover", e => {
+                let textBtn = btn.className.baseVal.replace("btn-", "");
+
+                this.execBtn();
+            });
+
+        });
+
+        this.addEventListenerAll(btn, "mouseover mouseup mousedown", e => {
+            btn, style.cursor = "pointer";
+        });
+
+    }
+
+
+    /* acrescentando método Set criado */
+    setDisplayDateTime() {
+        this.displayDate = this.currentDate.toLocaleDateString(this._locale, {
+            day: "2-digit",
+            month: "long",
+            year: "numeric"
+        });
+        this.displayTime = this.currentDate.toLocaleDateString(this._locale);
+
+    }
     get displayTime() {
-    return this._timeEl.innerHTML;
-}
+        return this._timeEl.innerHTML;
+    }
     set displayTime(value) {
-    return this._timeEl.innerHTML = value;
-}
+        return this._timeEl.innerHTML = value;
+    }
 
     get displayDate() {
-    return this._dateEl.innerHTML
-}
+        return this._dateEl.innerHTML
+    }
 
     set displayDate(value) {
-    return this._dateEl.innerHTML = value;
-}
+        return this._dateEl.innerHTML = value;
+    }
 
     get displayCalc() {
-    return this._displayCalcEl.innerHTML;
-}
+        return this._displayCalcEl.innerHTML;
+    }
     set displayCalc(value) {
-    return this._displayCalcEl.innerHTML = value;
+        return this._displayCalcEl.innerHTML = value;
 
-}
+    }
     get currentDate() {
-    return new Date();
-}
+        return new Date();
+    }
     set currentDate(value) {
-    return this._currentDate = value;
-}
+        return this._currentDate = value;
+    }
 
 }
